7caebd735808c978f7d5c3ec926649b6b4f30535
saqibmurtaza <saqibmurtazakhan@gmail.com>
2024-04-13 17:33:37 +0500

    Test Codes runs successfully

--- fastapi_endpoints_neondb/main.py
+++ fastapi_endpoints_neondb/main.py
@@ -5,7 +5,7 @@ from contextlib import asynccontextmanager
 from typing import Optional, Annotated
 from pydantic import BaseModel
 
-class Todo(SQLModel, table=True ):
+class Todo(SQLModel, table=True ):  ### Read Note:1
     id: Optional[int] = Field(default=None, primary_key=True)
     content:str = Field(index=True)
 
@@ -13,12 +13,12 @@ connection_string = str(settings.DATABASE_URL).replace(
     "postgresql", "postgresql+psycopg2"
 )
 
-engine = create_engine(connection_string, connect_args={"sslmode":"require"}, pool_recycle=300)
+engine = create_engine(connection_string, connect_args={"sslmode":"require"}, pool_recycle=300) ### Read Note:2
 
 def create_db_tables():
     SQLModel.metadata.create_all(engine)
 
-@asynccontextmanager
+@asynccontextmanager ### Read Note:3
 async def lifespan(app:FastAPI):
     print("Creating Database Tables ...") 
     create_db_tables()
@@ -33,7 +33,7 @@ app = FastAPI(
         "description" : " Development Server"
     }]
 )
-def get_session():
+def get_session(): ### Read Note:4
     with Session(engine) as session:
         yield session
 
@@ -43,7 +43,7 @@ def read_root():
     return message
 
 @app.post("/todo/")
-def create_todo(todo:Todo, session:Annotated[Session, Depends(get_session)]):
+def create_todo(todo:Todo, session:Annotated[Session, Depends(get_session)]): ### Read Note:5
 
     session.add(todo)
     session.commit()
@@ -57,7 +57,7 @@ def read_todo_list(session: Annotated[Session, Depends(get_session)]):
     return todos
 
 @app.delete("/todo/{todo_id}")
-def delete_todo(todo_id:int, todo:Todo, session:Annotated[Session, Depends(get_session)]):
+def delete_todo(todo_id:int, session:Annotated[Session, Depends(get_session)]): #Read Note:7
   db_todo = session.get(Todo, todo_id)
   if db_todo:
       session.delete(db_todo)
@@ -65,18 +65,13 @@ def delete_todo(todo_id:int, todo:Todo, session:Annotated[Session, Depends(get_s
       return {"message":"Todo deleted successfully"}
   else:
       raise HTTPException(status_code= 404, detail="Todo not found")
-'''
-1: Importing the BaseModel class from the pydantic module. BaseModel is the foundation for 
-creating Pydantic models.
-2: UpdateTodoRequest: Define a new Pydantic model called UpdateTodoRequest. This model has a 
-single field named content, which is of type str
-We use this model to validate and parse incoming todo-request-data
-'''
-class UpdateTodoRequest(BaseModel):
+
+### UPDATE_TODO_ENDPOINT
+class UpdateTodoRequest(BaseModel): ### Read Note:6
     content:str
 
 @app.put("/todo/{todo_id}")
-def update_todo(todo_id:int,todo_update:UpdateTodoRequest, session:Annotated[Session, Depends(get_session)]):
+def update_todo(todo_id:int,todo_update:UpdateTodoRequest, session:Annotated[Session, Depends(get_session)]): 
     todo = session.get(Todo, todo_id) #fetch existing todo by id
     if todo: # if true (not empty)
         todo.content = todo_update.content #update the todo content
@@ -87,5 +82,51 @@ def update_todo(todo_id:int,todo_update:UpdateTodoRequest, session:Annotated[Ses
         raise HTTPException(status_code=404, detail="Todo not Found")
 
 
-        
-        
+'''
+Note:1
+By specifying table=True, 
+--you are instructing the SQLModel to treat the class Todo as a database table.
+it typically triggers the creation of a corresponding database table based on the fields defined in the class.
+--The ORM framework will map the class attributes (fields) to table columns in the database. This mapping 
+allows you to work with objects of the Todo class in your Python code while transparently 
+interacting with database records.
+
+Note: 2
+In SQLAlchemy, the `Engine` is a central component responsible for database connection management. 
+It provides an interface for executing SQL queries and managing transactions. 
+The `create_engine()` function is used to create an `Engine` object,
+ which represents a connection to the database.
+
+ Note:3
+ An asynccontextmanager named lifespan:
+ It's used to perform initialization tasks before the application starts. 
+ In this case, it prints a message and creates database tables.
+
+ Note:4
+get_session() is a function that yields an SQLAlchemy session. It's used to manage database sessions.
+This function ensures that the session is properly closed after its use.
+session:
+represents a unit of work within a database connection. It encapsulates a series of database operations 
+(such as inserts, updates, deletes, or queries) that should be treated as a single transaction
+
+Note: 5
+The session dependency is used in the API endpoints to interact with the database. 
+It ensures that a session is created and passed to the endpoint functions.
+
+Note:6
+1: Importing the BaseModel class from the pydantic module. BaseModel is the foundation for 
+creating Pydantic models.
+2: UpdateTodoRequest: Define a new Pydantic model called UpdateTodoRequest. This model has a 
+single field named content, which is of type str
+We use this model to validate and parse incoming todo-request-data
+
+Note:7
+if you add parameter todo:Todo following issue be faced:
+To address the issue of encountering a 422 validation error during the pytest test for delete_todo, 
+specifically related to the todo: Todo parameter in the delete_todo function, you can modify the function
+to remove the todo: Todo parameter from it 
+{def delete_todo(todo_id:int, todo:Todo, session:Annotated[Session, Depends(get_session)]):}
+
+Delete requests typically do not require a request body, so we can simplify the endpoint to only 
+accept the todo_id for deletion.
+'''
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,32 +1,150 @@
 from fastapi.testclient import TestClient
-from fastapi_endpoints_neondb.main import app, get_session, engine, settings, Todo
-from sqlmodel import Session, create_engine, select, SQLModel
+from fastapi_endpoints_neondb.main import app, get_session, settings
+from sqlmodel import Session, create_engine, SQLModel
+import json
 
 ### FIRST TEST READ ROOT
 def test_read_root():
     client = TestClient(app=app)
-    response = client.get("/") # we generate https request to server & hold server-response in response
-    assert response.status_code == 200, "Status Code Failure" # assertion test: if server-response == 200 (Passesd) else: Status code failure
+    response = client.get("/")                                          # we generate https request to server & hold server-response in variable response
+    assert response.status_code == 200, "Status Code Failure"           # assertion test: if server-response == 200 (Passesd) else: Status code failure
     assert response.json() == {"Hello":"My First API Endpoint"}, "Server-Response != Content"
-
+    
 ### SECOND TEST CREATE_TODO
 def test_create_todo():
+    
     connection_string = (str(settings.TEST_DATABASE_URL).replace(
         "postgresql", "postgresql+psycopg2"))
-    
     engine = create_engine(connection_string, connect_args={"sslmode":"require"})
-    
-    SQLModel.metadata.create_all(engine) # automatically generate db tables, passing engine as an argument, which their have dtails of test db connection details
-
+    SQLModel.metadata.create_all(engine)                                # automatically generate db tables, passing engine as an argument, which their have dtails of test db connection details
     with Session(engine) as session:
-        def dependecy_override_function(): # return session which use engine of test_db_url
+        def dependecy_override_function():                              # return session which use engine of test_db_url
             return session
-    
     app.dependency_overrides[get_session] = dependecy_override_function
 
     client = TestClient(app=app)
-    todo_content = Todo(id=1, content="Buy Grocery") # initiate instance of Todo class
-    response = client("/todo/",json=todo_content) # generate http request to server with payload-json-request
+
+    todo_data = {"content":"buy Grocerry"}
+    response = client.post("/todo/",json=todo_data)                     # generate http request to server with todo_content as json-payload
+    assert response.status_code == 200
+
+    extract_content = response.content
+    print(f"extracted_content_from_response:{extract_content}")
+    
+    response_in_dict= json.loads(extract_content)                       ### Read Note:1
+    print(f"response_in_dict:{response_in_dict}")
+    
+    assert response_in_dict["content"] == todo_data["content"], "Assertion Test Failure, 'Content Not Found'"
+    assert "id" in response_in_dict, " Assertion Test Failure, 'id not found'" 
+
+                                                                        ### Read Note:2
+
+### THIRD TEST READ_TODO_LIST
+
+def test_read_todo_list():
+    
+    connection_string= str(settings.TEST_DATABASE_URL).replace(
+        "postgresql", "postgresql+psycopg2")
+    engine = create_engine(connection_string, connect_args={"sslmode":"require"},
+                            pool_recycle=300)
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        def dependency_override_function():
+            return session
+    app.dependency_overrides[get_session] = dependency_override_function
+    
+    client = TestClient(app=app)
+
+    response = client.get("/todo/")
+    response_in_json = response.json()
+    print(f"response_in_json:{response_in_json}")
+    
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+### FOURTH TEST DELETE_TODO
+def test_delete_todo():
+
+    connection_string= str(settings.TEST_DATABASE_URL).replace(
+        "postgersql", "postgresql+psycopg2")
+    engine = create_engine(connection_string, connect_args={"sslmode":"require"}, pool_recycle=300)
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        def dependency_override_function():
+            return session
+    app.dependency_overrides[get_session] = dependency_override_function
+
+    client = TestClient(app=app)
+
+    create_todo = {"content":"Buy coffee, milk & cream"}
+    response = client.post("/todo/", json=create_todo)
+    assert response.status_code == 200, "Assertion Test Failure to Post/Create Todo "
+    created_todo_id = response.json()["id"]
+    print(created_todo_id)
+
+    # Delete the created todo item
+    response = client.delete(f"/todo/{created_todo_id}")
+    
+    assert response.status_code == 200, "Assertion Test Failure to Delete Todo"
+    assert response.json() == {"message": "Todo deleted successfully"}, "Delete Response Message Mismatch"
+
+### FIFTH TEST DELETE_TODO
+def test_update_todo():
+
+    connection_string = str(settings.TEST_DATABASE_URL).replace(
+        "postgresql", "postgresql+psycopg2")
+    engine = create_engine(connection_string, connect_args={"sslmode":"require"}, pool_recycle=300)
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        def dependency_override_function():
+            return session
+    app.dependency_overrides[get_session] = dependency_override_function
+
+    client = TestClient(app=app)
+
+    create_todo = {"content":"Buy Toys, Xbox"}
+    response = client.post("/todo/", json=create_todo)
+    assert response.status_code == 200, "Assertion Test Failure to create_todo in Test_update"
+
+    created_todo_id = response.json()["id"]
+    print(f"create_todo_id:{created_todo_id}")
+
+    # Update the created Todo
+    todo_data = {"content":"Buy Jackets"}
+    response = client.put(f"/todo/{created_todo_id}", json=todo_data)
+    print(f"response:{response.json()}")
     
 
+    assert response.status_code == 200, "Assertion Test Failure"
+    assert response.json().get("message") == "Todo successfully updated", "Update response message mismatch"
+    
+    # assert response.json()["content"] == todo_data["content"]         ### Read Note:3
+ 
+    #################### NOTES #############################
+    '''
+    Note:1
+    json.loads() is used to parse (deserialize) the JSON-formatted bytes (or string) 
+    into a Python dictionary (dict)
+    - json.loads(), which expects a JSON-formatted string (str), bytes, or bytearray as input, 
+    not a Response object.
+    -extract the JSON content (response body) from the Response object returned by client.post() 
+    and then use json.loads() to deserialize it into a Python dictionary, because json.load doesn't 
+    accept obect (server-response is in json object)
 
+    Note:2 
+    Since the id field is optional in Todo model and is automatically generated upon insertion, 
+    & can rely on the database behavior to assign and return this value upon creation.
+    This approach allows you to validate that the created todo has the expected content 
+    and optionally verify the presence of an assigned 'id' in the response.
+
+    Note:3
+   "In the test, test_todo_update, we often encounter an assertion error (KeyError: 'content') 
+   when attempting to verify the updated todo item's content with 
+   assert response.json()["content"] == todo_data["content"]. 
+   
+   This error occurs because the response from a PUT (update) or DELETE request typically 
+   does not include the content of the updated or deleted item in the response body. 
+   Instead, these requests usually return a success message mentioned in your main.py code
+   like "message": "Todo deleted successfully" or {"message": "Todo successfully updated"} to 
+   indicate the success of the operation."
+    '''
